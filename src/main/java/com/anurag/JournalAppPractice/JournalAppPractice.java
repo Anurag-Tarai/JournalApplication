e4e8dfa2dc5c63f28572a15becf12ab834e052b9
Anurag Tarai <anuragtarai051@gmail.com>
2024-07-26 15:06:32 +0530

    Structure of the project updated!

---
+++ src/main/java/com/anurag/JournalAppPractice/JournalAppPractice.java
@@ -0,0 +1,13 @@
+package com.anurag.JournalAppPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalAppPractice {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalAppPractice.class, args);
+	}
+
+}
