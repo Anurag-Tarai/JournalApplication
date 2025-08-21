a48bb7754260e20793714577148141277875097b
Anurag-Tarai <anuragtarai051@gmail.com>
2025-08-21 21:34:43 +0530

    add /public endpoint for Render healthcheck

--- src/main/java/com/anurag/journalapp/controller/PublicController.java
+++ src/main/java/com/anurag/journalapp/controller/PublicController.java
@@ -1,37 +1,20 @@
-//package com.anurag.journalapp.controller;
-//
-//import com.anurag.journalapp.entity.Journal;
-//import com.anurag.journalapp.entity.User;
-//import com.anurag.journalapp.service.JournalEntryService;
-//import com.anurag.journalapp.service.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/public")
-//public class PublicController {
-//    @Autowired
-//    private UserService userService;
-//
-//    @Autowired
-//    private JournalEntryService journalEntryService;
-//
-//
-//    @GetMapping
-//    public String health_check(){
-//        return "OK";
-//    }
-//
-//    @PostMapping("/register")
-//    public ResponseEntity<User> add(@RequestBody User user){
-//        return userService.save(user);
-//    }
-//
-//    @GetMapping("/all_journals")
-//    public List<Journal> check() {
-//        return journalEntryService.all_journal();
-//    }
-//}
+package com.anurag.journalapp.controller;
+
+import com.anurag.journalapp.entity.Journal;
+import com.anurag.journalapp.entity.User;
+import com.anurag.journalapp.service.JournalEntryService;
+import com.anurag.journalapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+    @GetMapping
+    public String health_check(){
+        return "OK";
+    }
+}
